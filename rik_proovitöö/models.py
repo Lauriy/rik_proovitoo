49ad11086694f312e98acc7bf0a578c526a693d1
Lauri Elias <laurileet@gmail.com>
2024-07-22 13:39:41 +0300

    fix: move business logic validation to admin forms for now

--- rik_proovitöö/models.py
+++ rik_proovitöö/models.py
@@ -1,6 +1,3 @@
-from datetime import date
-
-from django.conf import settings
 from django.contrib.postgres.indexes import GinIndex
 from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
@@ -13,54 +10,6 @@ from django.utils.translation import gettext_lazy as _
 class CreatedUpdatedMixin:
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
-
-
-def _calculate_personal_check_digit(personal_code: int) -> int:
-    personal_code = str(personal_code)
-    first_tier_weights = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
-    second_tier_weights = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
-    first_tier_weight_sum = sum(
-        [
-            int(x) * first_tier_weights[i]
-            for i, x in enumerate(personal_code[:10])
-        ]
-    )
-    possible_check_digit = first_tier_weight_sum % 11
-    if possible_check_digit != 10:
-        return possible_check_digit
-    else:
-        second_tier_weight_sum = sum(
-            [
-                int(x) * second_tier_weights[i]
-                for i, x in enumerate(personal_code[:10])
-            ]
-        )
-        possible_check_digit = second_tier_weight_sum % 11
-        if possible_check_digit != 10:
-            return possible_check_digit
-        else:
-            return 0
-
-
-def _calculate_birth_year(personal_code: int) -> int:
-    personal_code = str(personal_code)
-    if personal_code[0] in ["1", "2"]:
-        year_digits = "18"
-    elif personal_code[0] in ["3", "4"]:
-        year_digits = "19"
-    else:
-        year_digits = "20"
-
-    return int(f"{year_digits}{personal_code[1:3]}")
-
-
-def _convert_to_birthdate(personal_code: int) -> date:
-    birth_year = _calculate_birth_year(personal_code)
-    personal_code = str(personal_code)
-    birth_month = int(personal_code[3:5])
-    birth_day = int(personal_code[5:7])
-
-    return date(year=birth_year, month=birth_month, day=birth_day)
 
 
 def no_future_date(value):
@@ -82,34 +31,6 @@ class LegalEntity(Model, CreatedUpdatedMixin):
 
     def __str__(self):
         return f'{self.name} ({self.code})'
-
-    # def save(self, *args, **kwargs):
-    #     self.full_clean()
-    #     super().save(*args, **kwargs)
-
-    def clean(self):
-        super().clean()
-        if self.is_person:
-            if len(str(self.code)) != 11:
-                raise ValidationError(_('Personal codes must be 11 digits long'))
-            is_check_digit_correct = _calculate_personal_check_digit(self.code) == int(str(self.code)[-1])
-            if not is_check_digit_correct:
-                raise ValidationError(_('Personal code check digit is incorrect'))
-            self.creation_date = _convert_to_birthdate(self.code)
-        else:
-            if len(str(self.code)) != 7:
-                raise ValidationError(_('Business codes must be 7 digits long'))
-            if not self.creation_date:
-                raise ValidationError(_('Businesses must have an establishment date specified'))
-            if not self.capital or self.capital < settings.LLC_MIN_CAPITAL:
-                raise ValidationError(_('Businesses must have at least %(min_capital)s capital') % {
-                    'min_capital': settings.LLC_MIN_CAPITAL})
-            total_equity = sum(equity.value for equity in self.stakes.all())
-            if total_equity != self.capital:
-                raise ValidationError(
-                    _('Total equity holdings (%(total_equity)s) do not match the company\'s capital (%(capital)s)') % {
-                        'total_equity': total_equity, 'capital': self.capital}
-                )
 
     class Meta:
         verbose_name = _('Legal entity')
